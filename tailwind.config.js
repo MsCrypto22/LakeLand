d5fc6510cf1d49ee51291a49fc07f6c32a2b159b
AntN1 <anega1@terpmail.umd.edu>
2024-08-05 14:39:22 -0400

    testing new bg image on homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         "custom-gradient":
-          'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("/assets/lakeland-map.jpeg")',
+          'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("/assets/Lakeland Map.jpeg")',
       },
     },
   },
